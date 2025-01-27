981c524524c595cb4fed517ac2f0ce4d06475546
Daniel Nickson <github@danielonline.me>
2025-01-27 13:27:47 +1030

    Update init to run setup_entry

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
 """The Zone Touch 3 Integration."""
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import 
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Example integration from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Add other setup code here
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id)
+
+    # Add other unload code here
+
+    return True
